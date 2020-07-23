7f3d7334247b4aa48c187c17df20ca242a74a367
tehilauzan <46137999+tehilauzan@users.noreply.github.com>
2020-07-23 10:40:22 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/itertols_tehila.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/itertols_tehila.dir/main.cpp.obj"
+  "itertols_tehila.exe"
+  "itertols_tehila.exe.manifest"
+  "itertols_tehila.lib"
+  "itertols_tehila.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/itertols_tehila.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
